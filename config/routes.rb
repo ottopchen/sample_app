7d826108d8b2aa1681e44a18d83819119f3527a2
Otto P Chen <ottopchen@gmail.com>
2017-01-03 12:15:35 -0800

    Add account activation

--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
   resources :users
+  resources :account_activations, only: [:edit]
 end
