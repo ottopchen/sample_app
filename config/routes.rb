ea409e1639a4e71a333cb156c597c340d889482f
Otto P Chen <ottopchen@gmail.com>
2017-01-10 00:11:26 -0800

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -20,8 +20,13 @@ Rails.application.routes.draw do
   delete '/logout',  to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 end
